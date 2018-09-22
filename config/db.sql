7e0cb32d082defc050777fda6b6415551995b516
HendrikEduard <hekuiper@gmail.com>
2018-09-22 09:03:53 +0200

    Update db.sql

--- config/db.sql
+++ config/db.sql
@@ -1,9 +1,12 @@
 CREATE TABLE `employees` (
-  `id` tinyint(11) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `name` varchar(255) NOT NULL,
-  `city` varchar(255) NOT NULL,
-  `designation` varchar(255) NOT NULL
-) ENGINE='InnoDB' COLLATE 'utf8mb4_unicode_ci';
+  `id` tinyint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `city` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `modified_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE VIEW `employee` AS
 SELECT * FROM employees WHERE is_deleted=0;
