940bc86aa3a6a48b99c2347a716d24c0d858c7a0
HendrikEduard <hekuiper@gmail.com>
2018-09-25 10:30:24 +0200

    Added create date

--- config/db.sql
+++ config/db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `Ghp02FgpuqfbeO`DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE `Ghp02FgpuqfbeO` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
 USE `Ghp02FgpuqfbeO`;
 
 CREATE TABLE `employees` (
@@ -6,10 +6,12 @@ CREATE TABLE `employees` (
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `city` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `modified_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 
 CREATE VIEW `employee` AS
 SELECT * FROM employees WHERE is_deleted=0;
